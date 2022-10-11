1eb467c3ad973f926b0757ce30e2efc4c23c565a
Naan Ahmed <naanahmed.na@gmail.com>
2022-10-11 22:42:32 +0300

    redo task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,25 +10,19 @@
   */
 void bubble_sort(int *array, size_t size)
 {
-	int i = 0, aux = 0, max = 0, swapped = 1;
+	size_t i, j, temp;
 
-	if (!array || size < 2)
-		return;
-
-	max = size - 1;
-
-	for (; i < max; ++i)
+	for (i = 0; i < size; i++)
 	{
-		if (array[i] > array[i + 1])
+		for (j = 1; j < size - i; j++)
 		{
-			aux = array[i];
-			array[i] = array[i + 1];
-			array[i + 1] = aux;
-			swapped = 1;
-			print_array(array, size);
+			if (array[j - 1] > array[j])
+			{
+				temp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = temp;
+				print_array(array, size);
+			}
 		}
-
-		if (swapped == 1 && i == max - 1)
-			i = -1, swapped = 0, --max;
 	}
 }
