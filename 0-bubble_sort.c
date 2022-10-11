4e1495f55fea7f82070ae190850d646d09faa0fe
Naan Ahmed <naanahmed.na@gmail.com>
2022-10-11 22:18:50 +0300

    Mandatory Tasks

---
+++ 0-bubble_sort.c
@@ -0,0 +1,22 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	int i = 0, aux = 0, max = 0, swapped = 1;
+	if (!array || size < 2)
+		return;
+	max = size - 1;
+	for (; i < max; ++i)
+	{
+		if (array[i] > array[i + 1])
+		{
+			aux = array[i];
+			array[i] = array[i + 1];
+			array[i + 1] = aux;
+			swapped = 1;
+			print_array(array, size);
+		}
+		if (swapped == 1 && i == max - 1)
+			i = -1, swapped = 0, --max;
+	}
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# 0x1B. C - Sorting algorithms & Big O Project Designed by Naan Mohammed
